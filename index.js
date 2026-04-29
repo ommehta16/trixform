b7e8f436b10d5cc31251db3d68b34dcd72022825
Om Mehta <ommehta16@icloud.com>
2026-04-29 17:17:02 -0400

    make points added form polyline, add nearest-point outlines

--- index.js
+++ index.js
@@ -43,28 +43,92 @@ const twoElement = document.querySelector(".twojs");
 const mouse = {x:0,y:0}
 const current = {x:0,y:0}
 
+const polylines = [];
 
-/** @param {PointerEvent} e */
-function pointerdown(e) {
+/**
+ * @param {PointerEvent} e
+ * @returns {{x:number, y:number}|null}
+ */
+function getMousePos(e) {
   if (!document.querySelector(".twojs:hover")) return;
   mouse.x = e.clientX;
   mouse.y = e.clientY;
 
   const twoOutsideRect = twoElement.getBoundingClientRect();
 
-  const relative = {
+  const rel = {
     x:mouse.x - twoOutsideRect.left - two.width*0.5,
     y:mouse.y - twoOutsideRect.top - two.height*0.5
   };
+  return rel;
+}
 
-  const circle = two.makeCircle(relative.x,relative.y,4);
-  circle.fill="black";
+/** @param {PointerEvent} e */
+function pointerdown(e) {
+  const rel = getMousePos(e);
+  if (!rel) return;
 
-  group.children.push(circle);
+  const path = polylines[polylines.length-1] ?? (() => {
+    const temp=two.makePath([]);
+    temp.fill="transparent";
+    temp.closed=false;
+    
+    polylines.push(temp);
+    group.children.push(temp);
+    return temp;
+  })();
+  
+  const anchor = new Two.Anchor(rel.x,rel.y, rel.x, rel.y, rel.x, rel.y);
+  path.vertices.push(anchor);
+
+  anchor.circle = two.makeCircle(rel.x,rel.y,2);
+  anchor.circle.fill="black";
+  group.children.push(anchor.circle);
+
+  two.update();
+}
+
+/**
+ * @param {{x:number, y:number}} a
+ * @param {{x:number, y:number}} b
+ */
+function distSq(a, b) {
+  return Math.pow(a.x-b.x,2) + Math.pow(a.y-b.y,2);
+}
+
+const hoverCircle = two.makeCircle(0,0,10);
+group.children.push(hoverCircle);
+
+/** @param {PointerEvent} e */
+function pointermove(e) {
+  const rel = getMousePos(e);
+  if (!rel) return;
+  if (!polylines.length) return;
+
+  let closestCircle = null;
+  let closestDist = Infinity;
+
+  for (const anchor of polylines[polylines.length-1].vertices) {
+    const dist = distSq(anchor, rel);
+    if (dist >= closestDist) continue;
+    
+    closestDist=dist;
+    closestCircle = anchor;
+  }
+
+  console.log(closestCircle, hoverCircle);
+
+  hoverCircle.position.set(closestCircle.x, closestCircle.y);
+  
+  hoverCircle.radius = 10;
+  hoverCircle.fill = "#3da6fd6b";
+  hoverCircle.stroke="transparent";
+
   two.update();
 }
 
 addEventListener("pointerdown",pointerdown);
+addEventListener("pointermove",pointermove);
 
 document.querySelector("#start").addEventListener("click",()=>{
   two.play();
